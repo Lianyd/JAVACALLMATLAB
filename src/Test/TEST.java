74260de4015df00ddf2bcbb92a8dc7187b76e619
liany <601292628@qq.com>
2018-04-12 17:18:37 +0800

    Initial commit

---
+++ src/Test/TEST.java
@@ -0,0 +1,10 @@
+import com.mathworks.toolbox.javabuilder.MWException;
+import showpic.Class1;
+public class TEST {
+    public static void main(String args[])throws MWException{
+        Class1 TUPIAN = new Class1();
+        TUPIAN.showpic("D:\\Matlab_Project\\test\\03.BMP");
+        Class1 TUPIAN1 = new Class1();
+        TUPIAN1.showpic("D:\\Matlab_Project\\test\\01.JPG");
+      }
+}
